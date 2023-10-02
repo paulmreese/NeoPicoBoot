99db034139379e5aa3e09f76bcad3dad5dc214cb
paulmreese <paul.m.reese@gmail.com>
2023-10-02 16:05:51 -0400

    Update build scripts

--- build-all.sh
+++ build-all.sh
@@ -10,6 +10,8 @@ test -d PicoWNeoPixelServer/pico-sdk || git clone --recursive https://github.com
 test -d PicoWNeoPixelServer/pico-sdk/FreeRTOS || git clone --recursive --branch smp https://github.com/FreeRTOS/FreeRTOS-Kernel PicoWNeoPixelServer/pico-sdk/FreeRTOS
 grep -e ip4_secondary_ip_address PicoWNeoPixelServer/pico-sdk/lib/lwip/src/core/ipv4/ip4.c || patch -p1 -d PicoWNeoPixelServer/pico-sdk/lib/lwip < PicoWNeoPixelServer/lwip_patch/lwip.patch || (echo "Failed to apply patch" && exit 1)
 
-echo "If the filesystem required (re)building, some values set as compiler options in CMakeLists.txt may be ignored. Please verify your settings, especially your SSID and Password"
+test -f src/ipl.h || ./process_ipl.py src/iplboot.dol src/ipl.h || { echo "IPL boot failed to process. Have you downloaded an 'iplboot.dol' file and copied it into the 'src' folder?"; exit 1; }
+
+echo "If the filesystem required (re)building, some values set as compiler options in CMakeLists.txt may be ignored. Please verify your settings, most importantly your SSID and Password"
 cmake -S . -B build -DPICO_SDK_PATH=`pwd`/pico-sdk -DPICO_GCC_TRIPLE=arm-none-eabi
 make -C build || {echo "Failed to build."; exit 1;}
